b5dbd0479c9fa71d3156105afa1d03750ea7d925
Clara Becker-Jostes <claramaria@MacBook-Air-tilhrende-Clara.local>
2023-02-21 19:19:34 +0100

    yarn added

--- app/src/components/messages.js
+++ app/src/components/messages.js
@@ -1,73 +1,89 @@
 import React, { useEffect, useCallback } from "react";
 import messagingService from "../services/messaging.service";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import Avatar from '@mui/material/Avatar';
-import { deepOrange, deepPurple } from '@mui/material/colors';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Avatar from "@mui/material/Avatar";
+import { deepOrange, deepPurple } from "@mui/material/colors";
 import { Typography } from "@mui/material";
 
 export default function Messages() {
+  const [messages, setMessages] = React.useState([]);
+  const [message, setMessage] = React.useState("");
+  const [messageToDisplay, setMessageToDisplay] = React.useState([]);
 
-    const [messages, setMessages] = React.useState([]);
-    const [message, setMessage] = React.useState("");
-    const [messageToDisplay, setMessageToDisplay] = React.useState([]);
-
-    const RetrieveMessages = useCallback(
-        async () => {
-            messagingService.findAll()
-                .then(response => {
-                    console.log(response)
-                    if (response.data.length > 0) {
-                        setMessages(response.data);
-                    }
-                })
-                .catch(e => {
-                    console.log(e);
-                });
-        },
-        [],
-    )
-
-    useEffect(() => {
-        RetrieveMessages()
-    }, [RetrieveMessages]);
-
-    useEffect(() => {
-        const messagesToDisplay = messages.map((message) => {
-            return (
-                <Stack direction='row' width="100%">
-                    <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
-                    <Typography variant="p" component="p">
-                        {message.Message_content}
-                    </Typography>
-                </Stack>
-            )
-        });
-        setMessageToDisplay(messagesToDisplay)
-    }, [messages]);
-
-    const postMessage = () => {
-        const messageBody = {
-            Message_content: message
+  const RetrieveMessages = useCallback(async () => {
+    messagingService
+      .findAll()
+      .then((response) => {
+        console.log(response);
+        if (response.data.length > 0) {
+          setMessages(response.data);
         }
-        messagingService.create(messageBody)
-            .then(response => {
-                console.log(response)
-                RetrieveMessages()
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
 
-    return (
-        <Stack spacing={2} direction="column" width="20%" margin="auto">
-            <Stack spacing={2} direction="column" width="20%" margin="auto" height="80vh">
-                {messageToDisplay}
-            </Stack>
-            <TextField id="standard-basic" value={message} onChange={(e) => setMessage(e.target.value)} label="Write message" variant="standard" sx={{ marginTop: "auto" }} />
-            <Button onClick={postMessage} variant="contained">Contained</Button>
+  useEffect(() => {
+    console.log("useEffect for retrieved messages");
+    RetrieveMessages();
+  }, [RetrieveMessages]);
+
+  useEffect(() => {
+    console.log("useEffect for msg");
+    const messagesToDisplay = messages.map((message) => {
+      return (
+        <Stack direction="row" width="100%">
+          <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
+          <Typography variant="p" component="p">
+            {message.Message_content}
+          </Typography>
         </Stack>
-    )
+      );
+    });
+    setMessageToDisplay(messagesToDisplay);
+  }, [messages]);
+
+  const postMessage = () => {
+    const messageBody = {
+      Message_content: message,
+    };
+    console.log("messagingService");
+    messagingService
+      .create(messageBody)
+      .then((response) => {
+        console.log("Respons fra server vha Axios %o", response);
+        RetrieveMessages();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <Stack spacing={2} direction="column" width="20%" margin="auto">
+      <Stack
+        spacing={2}
+        direction="column"
+        width="20%"
+        margin="auto"
+        height="80vh"
+      >
+        {messageToDisplay}
+      </Stack>
+      <TextField
+        id="standard-basic"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        label="Write message"
+        variant="standard"
+        sx={{ marginTop: "auto" }}
+      />
+      <Button onClick={postMessage} variant="contained">
+        Contained
+      </Button>
+    </Stack>
+  );
 }
--- app/src/services/messaging.service.js
+++ app/src/services/messaging.service.js
@@ -1,32 +1,30 @@
 import http from "./common.js";
 
 class MessagingService {
-
-    async create(body) {
-        try {
-            return await http.post(`/messages`, body);
-        } catch (error) {
-            console.log(error);
-        }
+  async create(body) {
+    console.log("Posting message?");
+    try {
+      return await http.post(`/messages`, body);
+    } catch (error) {
+      console.log(error);
     }
-    async findAll() {
-        try {
-            return await http.get(`/messages`);
-        }
-        catch (error) {
-            console.log(error);
-        }
+  }
+  async findAll() {
+    console.log("Finding messages");
+    try {
+      return await http.get(`/messages`);
+    } catch (error) {
+      console.log(error);
     }
-    async findOne(id) {
-        try {
-            return await http.get(`/messages/${id}`);
-        }
-        catch (error) {
-            console.log(error);
-        }
+  }
+  async findOne(id) {
+    try {
+      return await http.get(`/messages/${id}`);
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
 
 const messagingService = new MessagingService();
 export default messagingService;
-
